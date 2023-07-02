8ed40a2e0d077e13b159076ea904e3678e71feba
SesneiBunny <lukasmatrix12345@gmail.com>
2023-07-03 00:17:10 +0200

    BMI calculator back vol.1

--- app/src/main/java/eu/pl/snk/senseibunny/a7minuteapp/BmiActivity.kt
+++ app/src/main/java/eu/pl/snk/senseibunny/a7minuteapp/BmiActivity.kt
@@ -1,13 +1,21 @@
 package eu.pl.snk.senseibunny.a7minuteapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import eu.pl.snk.senseibunny.a7minuteapp.databinding.BmiScreenBinding
+import java.lang.Math.pow
+import kotlin.math.pow
 
 
 class BmiActivity : AppCompatActivity() {
     // you use name of xml activity here
     private var binding:BmiScreenBinding?=null
+
+    private var height:String?=null
+    private var weight:String?=null
+    private var bmi: Double?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +29,21 @@ class BmiActivity : AppCompatActivity() {
         binding?.toolBarBMI?.setNavigationOnClickListener{
             onBackPressed()
         }
+
+        binding?.calculate?.setOnClickListener{
+            calculate()
+        }
+
+    }
+
+    private fun calculate(){
+        height=binding?.etText2?.text.toString()
+        weight=binding?.etText1?.text.toString()
+
+        val sum = weight?.toDouble()?.div((height?.toDouble()!!).pow(2))?.times(10000)
+        Toast.makeText(this,height,Toast.LENGTH_LONG).show()
+        binding?.bmiRecord?.text=sum.toString().take(5)
+
 
     }
 
